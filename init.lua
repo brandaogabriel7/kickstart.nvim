98053ffde5992bdd701eb5ae051a200cc74b2f6d
Gabriel Brandão <contato@gabrielbrandao.net>
2025-06-18 16:34:14 -0300

    setup lualine plugin

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
