ab395b14f23f93bd783660514ad32fd1a5ac80f9
Gabriel Brandão <contato@gabrielbrandao.net>
2026-02-26 15:01:03 -0300

    feat: enable Copilot with toggle functionality
    Copilot now lazy-loads on InsertEnter, starts disabled by default,
    and can be toggled with <leader>cp or <C-\> in insert mode.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,1 +1,34 @@
-return { 'github/copilot.vim', enabled = false }
+local copilot_enabled = false
+
+local function toggle_copilot()
+  if copilot_enabled then
+    vim.cmd('Copilot disable')
+    copilot_enabled = false
+    vim.notify('Copilot disabled', vim.log.levels.INFO)
+  else
+    vim.cmd('Copilot enable')
+    copilot_enabled = true
+    vim.notify('Copilot enabled', vim.log.levels.INFO)
+  end
+end
+
+return {
+  'github/copilot.vim',
+  event = 'InsertEnter',
+  init = function()
+    vim.g.copilot_enabled = false
+  end,
+  keys = {
+    {
+      '<leader>cp',
+      toggle_copilot,
+      desc = 'Toggle Copilot',
+    },
+    {
+      '<C-\\>',
+      toggle_copilot,
+      mode = 'i',
+      desc = 'Toggle Copilot',
+    },
+  },
+}
