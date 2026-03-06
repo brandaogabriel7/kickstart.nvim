8fa21e69f78da97bd969652ee30fae7269f3ec7f
Gabriel Brandão <contato@gabrielbrandao.net>
2026-03-06 19:05:45 -0300

    feat: show relative filepath and cwd in statusline
    Display relative path in left section and current working directory
    name in right section of lualine.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,16 @@ return {
       options = {
         theme = 'dracula',
       },
+      sections = {
+        lualine_c = {
+          { 'filename', path = 1 }, -- 0=filename, 1=relative path, 2=absolute
+        },
+        lualine_z = {
+          function()
+            return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+          end,
+        },
+      },
     }
   end,
 }
